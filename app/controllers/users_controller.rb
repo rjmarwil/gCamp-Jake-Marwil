3ac4b0c15152d99463bbb7750f8abecc38fa82e5
Jake Marwil <rjm02006@gmail.com>
2015-02-04 15:51:51 -0700

    add delete option to edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,11 +36,9 @@ class UsersController < ApplicationController
 
 
   def destroy
+    @user = User.find(params[:id])
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to users_path, notice: 'User was successfully destroyed.'
   end
 
   private

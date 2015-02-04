62192306f1db4672613a27826662d94e9a5be160
Jake Marwil <rjm02006@gmail.com>
2015-02-04 15:29:09 -0700

    add update capability to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,27 +24,13 @@ class UsersController < ApplicationController
     else
       render :new
     end
-    # respond_to do |format|
-    #   if @user.save
-    #     format.html { redirect_to @user, notice: 'User was successfully created.' }
-    #     format.json { render :new, status: :created, location: @user }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @user.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
-
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'User was successfully updated.'
+    else
+      render :edit
     end
   end
 

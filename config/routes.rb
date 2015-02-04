3ab8e8c6708a4175bec614fdd1ff2904a51b7ae3
Jake Marwil <rjm02006@gmail.com>
2015-02-04 10:58:02 -0700

    add users index and allow users to add new users with form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :tasks
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -13,6 +14,7 @@ Rails.application.routes.draw do
   get '/about' => 'about#show'
   get '/terms' => 'terms#show'
   get '/tasks' => 'tasks#show'
+  get '/users' => 'users#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase

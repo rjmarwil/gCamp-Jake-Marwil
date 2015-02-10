29da0ee76da934bc9cb560f9814d8922a3493b0d
Jake Marwil <rjm02006@gmail.com>
2015-02-09 21:37:50 -0700

    add project controller with CRUD methods

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,54 @@
+class ProectsController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def edit
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @user.save
+      redirect_to projects_path, notice: 'Project was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to projects_path, notice: 'Project was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path, notice: 'Project was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_project
+      @project = Project.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def project_params
+      params.require(:project).permit(:name)
+    end
+end

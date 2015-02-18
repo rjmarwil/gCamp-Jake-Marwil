5348912b5822b52983b1d82ba018c8b812018b47
Jake Marwil <rjm02006@gmail.com>
2015-02-18 14:18:18 -0700

    configure login/register/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   get '/about' => 'about#show'
   get '/terms' => 'terms#show'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 

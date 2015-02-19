0c9d54a027b67cd7261e9bc0df788fac10e99bfc
Jake Marwil <rjm02006@gmail.com>
2015-02-18 19:16:36 -0700

    add registrations controller to allow users to signup

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,21 @@
+class RegistrationsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+   @user = User.new(params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation))
+   if @user.save
+     session[:user_id] = @user.id
+     redirect_to root_path
+   else
+     render :new
+   end
+ end
+
+ def signup
+   @user = User.new
+ end
+
+end

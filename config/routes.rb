4e9e704dd69e3671632461252a431318f4accb1c
Jake Marwil <rjm02006@gmail.com>
2015-03-11 15:39:15 -0600

    nest membership resources under projects

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :projects do
     resources :tasks
+    resources :memberships
   end
 
   # The priority is based upon order of creation: first created -> highest priority.

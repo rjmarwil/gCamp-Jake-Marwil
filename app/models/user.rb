80eded27b0bc10b06c6ec4f39d4e32dee842c118
Jake Marwil <rjm02006@gmail.com>
2015-03-11 15:24:24 -0600

    add has_many relationship to projects through memberships

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ActiveRecord::Base
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
 
+  has_many :memberships
+  has_many :projects, through: :memberships
+
   has_secure_password
 
 end

184d683b91ff279e644d1ca0310d74cc00100ead
Jake Marwil <rjm02006@gmail.com>
2015-04-07 15:32:01 -0600

    add dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ActiveRecord::Base
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
 
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :projects, through: :memberships
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :tasks, through: :comments
 
   has_secure_password

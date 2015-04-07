cd5ab9403b919362193efe8b3d5a6ba6864eadb3
Jake Marwil <rjm02006@gmail.com>
2015-04-07 15:51:57 -0600

    remove dependent destroy on comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   has_many :memberships, dependent: :destroy
   has_many :projects, through: :memberships
-  has_many :comments, dependent: :destroy
+  has_many :comments
   has_many :tasks, through: :comments
 
   has_secure_password

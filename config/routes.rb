33bce9155db0f05971d8e1f92175c8bc0717f545
Jake Marwil <rjm02006@gmail.com>
2015-04-07 14:12:01 -0600

    add resources for comments nested under tasks

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   resources :users
   resources :projects do
-    resources :tasks
+    resources :tasks do
+      resources :comments
+    end
     resources :memberships
   end
 

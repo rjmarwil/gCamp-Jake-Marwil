ee1dc4902f6bfe212fb744e20d2a411429ac7149
Jake Marwil <rjm02006@gmail.com>
2015-04-07 15:41:45 -0600

    add access to all database items

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,11 @@
 class AboutController < ApplicationController
 
   def show
+    @projects = Project.all
+    @tasks = Task.all
+    @memberships = Membership.all
+    @users = User.all
+    @comments = Comment.all
   end
 
 end

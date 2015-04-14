ff3542c283e60bf91277f17e37af134468055341
Jake Marwil <rjm02006@gmail.com>
2015-04-14 15:54:23 -0600

    add conditional for strong params to check if user is admin before allowing admin param

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,6 +58,10 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :admin)
+      if current_user.admin?
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :admin)
+      else
+        params.require(:user).permit(:first_name, :last_name, :email, :password)
+      end
     end
 end

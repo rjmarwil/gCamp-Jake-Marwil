de8983fda07ae4f49425a55f612d64184bf3fd63
Jake Marwil <rjm02006@gmail.com>
2015-04-16 10:59:02 -0600

    add test for project member cannot edit or delete project

--- spec/features/permissions_spec.rb
+++ spec/features/permissions_spec.rb
@@ -77,7 +77,19 @@ describe 'Certain action and user memberships allow different permissions' do
     expect(page).to have_content 'Member'
   end
 
-  it "a user cannot view projects they didn't create or aren't members of" do
+  it 'a user cannot view edit/delete actions for projects they are only a member of' do
+    # create a second project
+    @project2 = Project.create(name: "Example Project 2")
+    # assign @user to second project only as a member (role: 0)
+    Membership.create(user_id: @user.id, project_id: @project2.id, role: 0)
+    visit "/projects"
+    expect(page).to have_content 'Example Project 2'
+    first(:link, 'Example Project 2').click
+    expect(page).not_to have_content 'Edit'
+    expect(page).not_to have_content 'Delete'
+  end
+
+  it "a user cannot view projects they didn't create and aren't members of" do
 
   end
 

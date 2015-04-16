18e9b87a8b27fa38ae58b4fd1baeda8b672c10ea
Jake Marwil <rjm02006@gmail.com>
2015-04-16 10:55:40 -0600

    add test for users can view projects they are members of

--- spec/features/permissions_spec.rb
+++ spec/features/permissions_spec.rb
@@ -64,7 +64,17 @@ describe 'Certain action and user memberships allow different permissions' do
   end
 
   it 'a user can view projects they are a member of' do
-
+    # create a second project
+    @project2 = Project.create(name: "Example Project 2")
+    # assign @user to second project only as a member (role: 0)
+    Membership.create(user_id: @user.id, project_id: @project2.id, role: 0)
+    visit "/projects"
+    expect(page).to have_content 'Example Project 2'
+    first(:link, 'Example Project 2').click
+    expect(page).to have_content 'Example Project 2'
+    click_on "Memberships"
+    expect(page).to have_content 'Jake Marwil'
+    expect(page).to have_content 'Member'
   end
 
   it "a user cannot view projects they didn't create or aren't members of" do

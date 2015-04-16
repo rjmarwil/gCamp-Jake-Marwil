73a90d9264767778643d58b8a3c7fc127daae448
Jake Marwil <rjm02006@gmail.com>
2015-04-16 10:07:18 -0600

    pass test for newly registered users taken to new project page

--- spec/features/permissions_spec.rb
+++ spec/features/permissions_spec.rb
@@ -12,12 +12,25 @@ describe 'Certain action and user memberships allow different permissions' do
     fill_in "Email", with: 'rjm02006@gmail.com'
     fill_in "Password", with: 'password'
     click_on 'Sign In!'
-    expect(page).to have_content 'Welcome back, stranger!'
+    expect(page).to have_content 'You have successfully signed in'
 
   end
 
   it 'when user logs in, directed to projects index' do
     expect(page).to have_content 'Example Project'
+  end
+
+  it 'users who sign up are taken to project/new page' do
+    click_on 'Sign Out'
+    click_on 'Sign Up'
+    fill_in "user[first_name]", with: 'Bobo'
+    fill_in "user[last_name]", with: 'Johnson'
+    fill_in "user[email]", with: 'a@a.com'
+    fill_in "user[password]", with: 'a'
+    fill_in "user[password_confirmation]", with: 'a'
+    click_on "Sign Up!"
+    expect(page).to have_content "New Project"
+    expect(page).to have_content "Cancel"
   end
 
 end
